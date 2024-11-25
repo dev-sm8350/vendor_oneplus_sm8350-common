0f48ac4fc7a9b77c67aa9d7600d65db35b8d9c81
TogoFire <togofire@mailfence.com>
2024-11-25 18:58:36 -0300

    sm8350-common: Add missing vendor.qti.hardware.fm@1.0-impl libs
    * From: qssi-user 14 UKQ1.230924.001 1727597651382 release-keys
    * ref: https://dumps.tadiphone.dev/dumps/oneplus/oneplus9pro
    
    Log:
    E LegacySupport: Could not get passthrough implementation for vendor.qti.hardware.fm@1.0::IFmHci/default.
    
    Change-Id: 88c3c11c411f5880e4e61191b5885bbd2434d10e
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -1156,6 +1156,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.data.lce@1.0 \
     vendor.qti.hardware.data.qmi@1.0 \
     vendor.qti.hardware.fm@1.0 \
+    vendor.qti.hardware.fm@1.0-impl \
     vendor.qti.hardware.iop@2.0 \
     vendor.qti.hardware.mwqemadapter@1.0 \
     vendor.qti.hardware.qccsyshal@1.0 \
